1ac1b3cfe8972956aa8c56a95296868311c761a5
Zachary Kahlig <zkahlig64@gmail.com>
2023-06-19 11:38:02 -0400

    add first draft of contact us content

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,14 +1,59 @@
 import './style.css';
 
-export function createContactTab() {
-    const contactDiv = document.createElement('div');
-    contactDiv.classList.add('tab-content');
+function createElement(tag, attributes, textContent) {
+    const element = document.createElement(tag);
+    
+    if (attributes) {
+        Object.keys(attributes).forEach(key => {
+            element.setAttribute(key, attributes[key]);
+        });
+    }
+    
+    if (textContent) {
+        element.textContent = textContent;
+    }
+    
+    return element;
+}
 
-    const heading = document.createElement('h2');
-    heading.textContent = 'Contact Us';
+function appendChildren(parent, ...children) {
+    children.forEach(child => {
+        parent.appendChild(child);
+    });
+}
 
-    // Add more content...
+export function createContactTab(contactContent) {
+    const contactDiv = createElement('div', { class: 'tab-content contact' });
 
+    const heading = createElement('h2', null, 'Contact Us');
     contactDiv.appendChild(heading);
+
+    // Address Section
+    const addressSection = createElement('section', { class: 'address-section' });
+    const addressHeading = createElement('h3', null, contactContent.addressHeading);
+    const address = createElement('p', null, contactContent.address);
+    appendChildren(addressSection, addressHeading, address);
+
+    // Hours Section
+    const hoursSection = createElement('section', { class: 'hours-section' });
+    const hoursHeading = createElement('h3', null, contactContent.hoursHeading);
+    const hours = createElement('p', null, contactContent.hours);
+    appendChildren(hoursSection, hoursHeading, hours);
+
+    // Contact Form Section
+    const contactFormSection = createElement('section', { class: 'contact-form-section' });
+    const formHeading = createElement('h3', null, contactContent.formHeading);
+    const contactForm = createElement('form');
+    const inputName = createElement('input', { type: 'text', placeholder: 'Your Name' });
+    const inputEmail = createElement('input', { type: 'email', placeholder: 'Your Email' });
+    const textarea = createElement('textarea', { placeholder: 'Your Message' });
+    const submitButton = createElement('button', { type: 'submit' }, 'Send');
+    appendChildren(contactForm, inputName, inputEmail, textarea, submitButton);
+    appendChildren(contactFormSection, formHeading, contactForm);
+
+    // Appending all sections to contactDiv
+    appendChildren(contactDiv, addressSection, hoursSection, contactFormSection);
+
     return contactDiv;
 }
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,58 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
-   module: {
+  module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        test: /\.json$/,
+        loader: 'json-loader',
+        type: 'javascript/auto',
       },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }],
+          ['svgo', { plugins: [{ removeViewBox: false }] }],
+        ],
+      },
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+      new CssMinimizerPlugin(),
     ],
   },
 };
